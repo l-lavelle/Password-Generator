2fa162eb1ce56e11ceae12aed9ad6887626a1b65
Lauren <lauren.m.lavelle@wmich.edu>
2023-06-13 20:15:54 -0400

    character types in alert messages created

--- script.js
+++ script.js
@@ -3,19 +3,29 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   var char = window.prompt(
     "How many characters needed in password?\nEnter number between 8-128",
     "123"
   );
-  var lowerCase = alert("Do you want to include lower case letters?");
+  var lowerCase = window.confirm("Do you want to include lowercase letters?");
+  var upperCase = window.confirm("Do you want to include uppercase letters?");
+  var numbers = window.confirm("Do you want to include numbers?");
+  var specialCharcters = window.confirm(
+    "Do you want to include special characters?"
+  );
   console.log(lowerCase);
+  console.log(upperCase);
+  console.log(numbers);
+  console.log(specialCharcters);
+
   passwordText.value = password;
 }
-// Add in exceptions-
-// add if choose certain elements
+// Imput validated and at least one character type selected
+// password generated and matches selected criteria
+// passowrd is displayed
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
